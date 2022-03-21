b4a0a86fe65e677254b27b30f920955f75201dd3
KatieGott <Katie.Gott@target.com>
2022-03-21 15:29:55 -0500

    removed images

--- src/Card.js
+++ src/Card.js
@@ -3,9 +3,9 @@ import React from "react";
 export default function Card() {
     return (
         <div className="card">
-            <img src="..public/katie.png" className="card--image" />
+            <img src="..images/katie.png" className="card--image" />
             <div className="card--stats">
-                <img src="..public/star.png" className="card--star"/>
+                <img src="..imagesc/star.png" className="card--star"/>
                 <span>5.0</span>
                 <span className="gray">(6) * </span>
                 <span className="gray">USA</span>
--- src/Hero.js
+++ src/Hero.js
@@ -4,7 +4,7 @@ import React from "react"
 export default function Hero() {
     return(
         <section className="hero">
-            <img src="./public/collage1-bdf1703f540e4b538ae13f87b62bf3b5.webp" className="hero--photo" alt=""/>
+            <img src="./images/collage1-bdf1703f540e4b538ae13f87b62bf3b5.webp" className="hero--photo" alt=""/>
             <h1 className="hero--header">Online Experiences</h1>
             <p className="hero--text">Join unique interactive activities led by one of a kind hosts 
                 - all without leaving home.</p>
